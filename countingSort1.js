9b7030453a9f4e457a7c29f2954496da3e0967f7
julord87 <julianmzg@gmail.com>
2024-05-16 18:14:14 -0300

    counting sort 1

---
+++ countingSort1.js
@@ -0,0 +1,13 @@
+function countingSort(arr) {
+    let res = [];
+    
+    for(let i = 0; i < 100; i++) {
+        res[i] = 0;
+    }
+    
+    for(let j = 0; j < arr.length; j++) {
+        res[arr[j]] += 1
+    }
+    
+    return res;
+}
