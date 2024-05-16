23f4cd8048e8989e28fe208737eabc3a129b6145
julord87 <julianmzg@gmail.com>
2024-05-16 18:02:06 -0300

    diagonal difference

---
+++ diagonalDifference.js
@@ -0,0 +1,19 @@
+function diagonalDifference(arr) {
+    let firstDiag = 0;
+    let secDiag = 0;
+    let count = 0;
+    
+    for(let i = 0; i < arr.length; i++) {
+        let nArrLength = arr[i].length - 1;
+        
+        firstDiag += arr[i][i]
+        secDiag += arr[i][nArrLength - count]
+        count++
+    }
+    
+    if(firstDiag > secDiag) {
+        return firstDiag - secDiag
+    } else {
+        return secDiag - firstDiag
+    }
+}
