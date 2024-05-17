eb6a11b292d5f95193bb4a30396be52d343fba33
julord87 <julianmzg@gmail.com>
2024-05-17 12:16:44 -0300

    permuting two arrays

---
+++ pangrams.js
@@ -0,0 +1,13 @@
+function pangrams(s) {
+    const lowStr = s.toLowerCase();
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    let alphabetSet = new Set();
+
+    for (let char of lowStr) {
+        if (alphabet.includes(char)) {
+            alphabetSet.add(char);
+        }
+    }
+
+    return alphabetSet.size === 26 ? "pangram" : "not pangram";
+}
---
+++ permutingTwoArrays.js
@@ -0,0 +1,10 @@
+function twoArrays(k, A, B) {
+    const sortedA = A.sort((a, b) => a - b)
+    const sortedB = B.sort((a, b) => b - a)
+    
+    for(let i = 0; i < A.length; i ++) {
+        if(sortedA[i] + sortedB[i] < k) return "NO"
+    }
+    
+    return "YES"
+}
