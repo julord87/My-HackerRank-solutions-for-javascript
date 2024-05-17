aac9c4e921547d1ce66861aacd7e91c3774f5dc1
julord87 <julianmzg@gmail.com>
2024-05-17 17:05:44 -0300

    sub array division 1

---
+++ subArrayDivision1.js
@@ -0,0 +1,13 @@
+function birthday(s, d, m) {
+    let count = 0;
+
+    for(let i = 0; i <= s.length - m; i++) {
+        let tempSum = 0;
+        for(let j = 0; j < m; j++ ) {
+            tempSum += s[i + j];
+        }
+        if(tempSum === d) count++;
+    }
+
+    return count;
+}
