c5c484f7be3e0bd683024851a9a011992a72ce62
julord87 <julianmzg@gmail.com>
2024-05-21 17:04:48 -0300

    drawing book

---
+++ drawingBook.js
@@ -0,0 +1,6 @@
+function pageCount(n, p) {
+    const fromStart = Math.floor(p / 2)
+    const totalTurns = Math.floor(n / 2)
+    const fromEnd = Math.ceil(totalTurns - p / 2)
+    return Math.min(fromStart, fromEnd)
+}
