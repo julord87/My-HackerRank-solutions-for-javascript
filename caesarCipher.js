8e53befc912cb74bb39363b5950667cb1874c4ed
julord87 <julianmzg@gmail.com>
2024-05-29 17:45:16 -0300

    caesar's cipher option 2

--- caesarCipher.js
+++ caesarCipher.js
@@ -25,3 +25,31 @@ function caesarCipher(s, k) {
     
         return rotatedString;
 }
+
+
+
+// Option 2 //
+
+function caesarCipher(s, k) {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    const alphabetLength = alphabet.length;
+    k = k % alphabetLength;
+    let result = "";
+
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+        let isMayus = char === char.toUpperCase();
+        char = char.toLowerCase();
+
+        if (alphabet.includes(char)) {
+            let currentIndex = alphabet.indexOf(char);
+            let newIndex = (currentIndex + k) % alphabetLength;
+            let newChar = alphabet[newIndex];
+
+            result += isMayus ? newChar.toUpperCase() : newChar;
+        } else {
+            result += s[i];
+        }
+    }
+    return result;
+}
