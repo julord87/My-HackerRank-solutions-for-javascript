91b38ff964d61943e8afb58360bbc7abd877cf4f
julord87 <julianmzg@gmail.com>
2024-05-31 19:15:04 -0300

    sherlock and array option 2

--- sherlockAndArray.js
+++ sherlockAndArray.js
@@ -11,3 +11,27 @@ function balancedSums(arr) {
     };
     return 'NO';
 }
+
+
+// less efficient solution:
+
+function balancedSums(arr) {
+    
+    for(let i = 0; i < arr.length; i++) {
+        let leftSum = 0;
+        let rightSum = 0;
+        
+        for(let j = 0; j < arr.length; j++) {
+            let currentValue = arr[j];
+            if(j < i) {
+                leftSum += currentValue;
+            } else if(j > i) {
+                rightSum += currentValue;
+            }
+        }
+        
+        if(leftSum === rightSum) return "YES"
+    }
+    
+    return "NO"
+}
