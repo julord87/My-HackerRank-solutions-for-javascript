d7e313c18f5c9503f6359b2bbbea19ca9a8282ff
julord87 <julianmzg@gmail.com>
2024-05-31 18:48:20 -0300

    grid challenge option 2

--- gridChallenge.js
+++ gridChallenge.js
@@ -12,3 +12,28 @@ function gridChallenge(grid) {
     }
     return "YES";
 }
+
+
+// Option 2
+
+function gridChallenge(grid) {
+    const sortedGrid = [];
+    
+    for(let string of grid) {
+        sortedGrid.push(sortString(string));
+    }
+    
+    for(let i = 0; i < sortedGrid[0].length; i++) {
+        let currentStr = "";
+        for(let j = 0; j < sortedGrid.length; j ++) {
+            currentStr += sortedGrid[j][i]
+        }
+        if(currentStr !== sortString(currentStr)) return "NO"
+    }
+
+    return "YES"
+}
+
+function sortString(str) {
+    return str.split('').sort().join('')
+}
