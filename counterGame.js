de4c70dabc3a387c6ce1328431144d610150ca3c
julord87 <julianmzg@gmail.com>
2024-06-03 18:05:08 -0300

    renaming

--- counterGame.js
+++ counterGame.js
@@ -2,14 +2,14 @@ function counterGame(n) {
     let turn = 0;
 
     while (n > 1) {
-        if ((n & (n - 1)) === 0) {
+        if ((n & (n - 1)) === 0) { // Acá determinamos si n es potencia de 2
             n = n / 2;
         } else {
             let highestPowerOf2 = 1;
-            while (highestPowerOf2 * 2 < n) {
+            while (highestPowerOf2 * 2 < n) { // Buscamos la mayor potencia de 2 menor que n
                 highestPowerOf2 *= 2;
             }
-            n -= highestPowerOf2;
+            n -= highestPowerOf2; // Restamos la mayor potencia de 2 a n
         }
         turn++;
     }
